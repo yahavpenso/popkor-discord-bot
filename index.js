f817d0abc773af05c266cbd28a801e80d0e13e82
yahavpenso <103932678+yahavpenso@users.noreply.github.com>
2024-06-19 17:43:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require(' discord.js');
+
+const client = new Client({
+      intents: [
+          IntentsBitField.Flags.Guilds,
+          IntentsBitField.Flags.GuildMembers,
+         IntentsBitField.Flags.GuildMessages,
+         IntentsBitField. Flags.MessageContent,
