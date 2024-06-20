dfdbd9326918cdd84bda66f547e836c3674659a9
yahavpenso <103932678+yahavpenso@users.noreply.github.com>
2024-06-20 08:22:13 +0300

    Update register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -1,2 +1,10 @@
 require('dotenv').config();
 const { REST, Routes} = require('discord.js');
+
+const command = [
+    {
+      name:'hi',
+      description: 'Replies with hey!',
+    },
+];
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
