56f7fd0afe20a133671aec58649787938c93f6f3
yahavpenso <103932678+yahavpenso@users.noreply.github.com>
2024-06-24 12:03:55 +0300

    Update newFile.js

--- newFile.js
+++ newFile.js
@@ -1,1 +1,21 @@
+const { Routes } = require('discord.js');
+const { rest } = require('./register-commands');
+
+(async () => {
+    try {
+        console.log('Registering slash commands...');
+
+        await rest.put(
+            Routes.applicationGuildCommand(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            { body: commands }
+        );
+
+        console.log('Slash commands were registered successfully!');
+    } catch (error) {
+        console.log(`There was an error: ${error}`);
+    }
+})();
 
